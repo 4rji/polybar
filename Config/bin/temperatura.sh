440bb97631428878085c63cf10b3fd0496cfbc08
4rji <118249932+4rji@users.noreply.github.com>
2024-03-29 02:21:01 -0500

    Update temperatura.sh

--- Config/bin/temperatura.sh
+++ Config/bin/temperatura.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-temperature=$(sensors | grep "Package id 0:" | tr -d '+' | awk '{print $4}')
-icon=""  # Ejemplo de un icono de termómetro de Nerd Fonts
+#temperature=$(sensors | grep "Package id 0:" | tr -d '+' | awk '{print $4}')
+#icon=""  # Ejemplo de un icono de termómetro de Nerd Fonts
 
-echo "$temperature $icon"
+#echo "$temperature $icon"
+curl -s https://ipinfo.io/json | grep region | cut -d '"' -f 4
